3185406cafd2715cb2a8068c798d8f34c6da6378
Wanjoplang <wwansai@gmail.com>
2021-01-28 19:35:18 +0530

    Page not found

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-// ======== EVENTS ===================== //
-$(".view-topics").on("click",function(){
-    window.open("pages/categories.html","_self");
-});
-
-// ======== FUNCTIONS ================== //
